ab01e5a523addb7c9b6bf1ba782d48d09e02b5a8
User <user@usermail.com>
2021-08-07 09:42:15 +0200

    Create blank script

---
+++ wanikaniBurnReviews2.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Wanikani Burn Reviews 2
+// @namespace   wkburnreview2
+// @description Adds a space on the main page that reviews random burned items.
+// @version     0.0.1
+// @author      Byron Antak
+// @license     Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0); http://creativecommons.org/licenses/by-nc/4.0/
+// @include     http://www.wanikani.com/
+// @include     https://www.wanikani.com/
+// @include     http://www.wanikani.com/dashboard
+// @include     https://www.wanikani.com/dashboard
+// @require     https://greasyfork.org/scripts/19781-wanakana/code/WanaKana.js?version=126349
+// @grant       none
+// @require     https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    unsafeWindow.alert('Hello')
+})();

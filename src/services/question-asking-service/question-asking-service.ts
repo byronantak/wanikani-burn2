521c049f4132d739ae6a3757166b8438af0a4e84
User <user@usermail.com>
2021-08-28 09:45:35 +0200

    Add more typing

--- src/services/question-asking-service/question-asking-service.ts
+++ src/services/question-asking-service/question-asking-service.ts
@@ -1,9 +1,9 @@
 /// <reference path="../api/wani-kani-api-service.ts" />
 
 namespace Services {
-    const questionTypes = {
-        meaning: 'meaning',
-        reading: 'reading',
+    enum QuestionType {
+        Meaning = 'meaning',
+        Reading = 'reading',
     }
 
     export class QuestionAskingService {
@@ -15,26 +15,26 @@ namespace Services {
             this.waniKaniApiService = new Services.WaniKaniApiService(apiKey);
         }
 
-        randomizeSubject(subjectCount) {
+        public randomizeSubjectId(subjectCount: number): number {
             return Utilities.getRandomInt(0, subjectCount);
         }
 
-        randomizeQuestionType() {
+        public randomizeQuestionType(): QuestionType {
             const randomInt = Utilities.getRandomInt(0, 1);
             if (randomInt === 0) {
-                return questionTypes.meaning;
+                return QuestionType.Meaning;
             }
-            return questionTypes.reading;
+            return QuestionType.Reading;
         }
 
-        async doSomething() {
+        async doSomething(): Promise<void> {
             window.alert(`You API Key is: '${this.apiKey}'`);
             const userLevel = await this.waniKaniApiService.getUserLevel();
             Utilities.setValue(Constants.localStorageKeys.userLevel, String(userLevel));
             console.log('getUserLevel()', userLevel);
             const burnFilter = Utilities.getValue(Constants.localStorageKeys.filter, Models.BurnFilter.default({ userLevel }));
             const values = await this.waniKaniApiService.getAllBurnProgressionItems(burnFilter);
-            const randomIndex = this.randomizeSubject(values.length);
+            const randomIndex = this.randomizeSubjectId(values.length);
             const randomizedBurnedSubject = values[randomIndex];
             const subject = await this.waniKaniApiService.getSubjectDetails(randomizedBurnedSubject.subjectId);
             const questionType = this.randomizeQuestionType();
